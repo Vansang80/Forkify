1f91b3fdd7844cf60a876a391b2d83712154137f
Pascal Nguyen <pasngu@student.42.fr>
2019-03-26 14:33:20 +0100

    forkity

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,34 @@
-import str from './models/Search'
+import Search from './models/Search'
 
-import { add, multiply, ID } from './views/searchView'
+/**
+ * - Search objet
+ * -Current recipe object
+ * Shopping list object
+ * liked recipes
+ */
 
-console.log(ID)
-console.log(add(2, 5))
+const state = {}
+
+const controleSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza' // TODO
+
+    if (query) {
+        // 2 New search object and add to state
+        state.search = new Search(query)
+
+        // 3 Prepare UI for results
+
+        // 4 Search for recipes
+        await state.search.getResults()
+
+        // 5 render results on UI
+        console.log(state.search.result)
+    }
+
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault()
+    controleSearch()
+})
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,24 @@
-export default 'I am an exported string'
+import axios from 'axios'
+
+import cors from 'cors'
+cors()
+
+export default class Search{
+    constructor(query) {
+        this.query = query
+    }
+
+    async getResults() {
+        const key = '00a31211151627bc4cbbe1dcbee2cb0d'
+        
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+            this.result = res.data.recipes
+        
+        } catch(error) {
+            alert(error)
+        }
+    }
+}
+
+
