8be67f8ca3f6ba885d0241ae09a0d508f4c054a8
Pascal Nguyen <pasngu@student.42.fr>
2019-04-04 00:59:26 +0200

    forkity

--- src/js/index.js
+++ src/js/index.js
@@ -29,13 +29,17 @@ const controleSearch = async () => {
         searchView.clearResults()
         renderLoader(elements.searchRes)
 
-
-        // 4 Search for recipes
-        await state.search.getResults()
-
-        // 5 render results on UI
-        clearLoader()
-        searchView.renderResults(state.search.result)
+        try {
+            // 4 Search for recipes
+            await state.search.getResults()
+    
+            // 5 render results on UI
+            clearLoader()
+            searchView.renderResults(state.search.result)
+        } catch(err) {
+            alert('Something wrong')
+            clearLoader()
+        }
     }
 
 }
@@ -60,6 +64,31 @@ elements.searchResPages.addEventListener('click', e => {
     Controller Recipe
 */
 
-const r = new Recipe(35477)
-console.log(r.getRecipe())
-console.log(r)
+const controlRecipe = async () => {
+    // Get Id from url
+    const id = window.location.hash.replace('#', '')
+    console.log(id)
+
+    if (id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id)
+        
+        try {
+            // Get recipe data
+            await state.recipe.getRecipe()
+    
+            // Calculate servings and time
+            state.recipe.caclTime()
+            state.recipe.caclServings()
+    
+            // Render recipe
+            console.log(state.recipe)
+        } catch(error) {
+            alert('Error processing')
+        }
+    } 
+} 
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe))
